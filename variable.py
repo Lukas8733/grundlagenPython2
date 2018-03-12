e56e663ebef64ca25388137aaca90d0aa9b0e52b
Lukas8733 <171wita05@o365.htl-leoben.at>
2018-03-12 14:39:49 +0100

    variable

--- variable.py
+++ variable.py
@@ -1,1 +1,21 @@
 # variable
+
+a = 10
+b = 20
+
+summe = a + b
+
+print("Summe von", a, " + ", b, "=" , summe)
+
+aktion = input("Welche Aktion willst du? (+/-/*/:)")
+print("Aktion", aktion,"wird ausgeführt...")
+if(aktion == "*"):
+    print("Multiplikation ausgewählt")
+zahl = input("Welche zahl wählst du?")
+zahl = int(zahl)
+
+zahl2 = int("23")
+
+zahlA = "123"
+zahlB = "456"
+print(zahlA+zahlB)
